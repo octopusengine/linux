a945282f4ea1c402e4ea1b127a42a10331471be7
Honza S. Copak <bonum.comunae@volny.cz>
2017-10-22 16:53:37 +0200

    Update oe.sh

--- sh/oe.sh
+++ sh/oe.sh
@@ -13,10 +13,11 @@ fi
 if [[ $1 = "i" ]]; then
   echo "----- octopus engine info -----"
   uname -a
-  w=´whoami´
-  echo "whoami > $w" 
-  d=´pwd´
-  echo "pwd > $d"  
+  w=whoami
+  echo "whoami > `$w`" 
+  d=pwd
+  echo "pwd > `$d`"
+  sleep 1s  
 fi
 
 price1=123
@@ -24,13 +25,16 @@ if [[ $2 = "b" ]]; then
   echo "----- BTC/LTC -----"
   price1=$(wget -qO- https://www.bitstamp.net/api/v2/ticker/btcusd/ | grep -E -o 'last": "[0-9.]+"' | grep -E -o '[0-9]+.[0-9]{2}')
   echo "$price1 BTC/USD"
+  sleep 1s
   price2=$(wget -qO- https://api.bitfinex.com/v1/pubticker/ltcusd | grep -E -o 'last_price":"[0-9.]+"' | grep -E -o '[0-9.]+')
   echo "$price2 LTC/USD"
+  sleep 1s
 fi
 
 if [[ $2 = "c" ]]; then
   echo "----- crypto -----"
-  
+  echo -n "Test Hash" | sha256sum 
+  i=0; while ! (echo -n "OctopusEngine$i" | sha256sum | tr -d "\n"; echo " (nonce=$i)")|grep -E "^00"; do let i++; done  
 fi
 
 if [[ $2 = "m" ]]; then
