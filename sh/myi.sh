75c962c99b7c2ff75337588e23f61d998fadce6f
Honza S. Copak <bonum.comunae@volny.cz>
2021-07-14 21:53:42 +0200

    install and tmux .sh

---
+++ sh/myi.sh
@@ -0,0 +1,12 @@
+echo "myi.sh > my install - octopusLab test 2019"
+# sudo root ...
+
+apt install tmux htop iotop iftop tree scrot neofetch
+# lsusb lsmod 
+apt install nmap
+
+
+apt install gimp vlc cheese 
+
+
+ 
---
+++ sh/mytmux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ver. 0.2 - 2017
+# /home/yenda/mysh/mytmux.sh
+
+tmux has-session -t dev
+if [ $? != 0 ]
+then
+    tmux new-session -s dev -n "tmux.02/2017" -d
+    
+    tmux split-window -h
+    tmux split-window -dv
+    tmux split-window -dh
+    tmux split-window -h
+    tmux split-window -v
+    
+    tmux select-pane -t dev:0.0
+    tmux resize-pane -L 65
+    tmux send-keys 'tree' C-m ' neofetch' C-m
+    
+    tmux send-keys -t dev:0.1 'htop' C-m
+    tmux send-keys -t dev:0.2 'echo "22"' C-m
+    tmux send-keys -t dev:0.3 'watch -n1 "cat /proc/cpuinfo | grep "MHz" | sort -r | head -32"' C-m
+    tmux send-keys -t dev:0.4 'cmatrix' C-m
+    tmux send-keys -t dev:0.5 'watch -n 30 /home/yenda/mysh/btc.sh price btc' C-m
+    #sl sleep 2 
+    #tmux select-window -t dev:0.0 
+    #tmux selectp -t'{top-left}' 
+    tmux select-pane -t dev:0.0
+
+fi
+tmux attach -t dev
