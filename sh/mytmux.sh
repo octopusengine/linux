2ba69534f94d276fc7191d7309dfb2c2fe6c6ee0
Honza S. Copak <bonum.comunae@volny.cz>
2021-07-15 13:07:45 +0200

    Add files via upload

--- sh/mytmux.sh
+++ sh/mytmux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
-# ver. 0.2 - 2017
+#-----------------------
+ver="tmux 0.5/2017"
 # ${HOME}/mysh/mytmux.sh
 
 tmux has-session -t dev
 if [ $? != 0 ]
 then
-    tmux new-session -s dev -n "tmux.02/2017" -d
+    tmux new-session -s dev -n "$ver" -d
     
     tmux split-window -h
     tmux split-window -dv
@@ -16,10 +16,10 @@ then
     
     tmux select-pane -t dev:0.0
     tmux resize-pane -L 65
-    tmux send-keys 'tree' C-m ' neofetch' C-m
+    tmux send-keys 'tree' C-m 'neofetch' C-m
     
     tmux send-keys -t dev:0.1 'htop' C-m
-    tmux send-keys -t dev:0.2 'echo "octopusengine[dot]com is a community of Makers" | pv -qL 10' C-m
+    tmux send-keys -t dev:0.2 'echo "octopusengine[dot]com is a community of Makers" | pv -qL 10' C-m 'python3 $HOME/mysh/octopus.py' C-m
     tmux send-keys -t dev:0.3 'watch -n1 "cat /proc/cpuinfo | grep "MHz" | sort -r | head -32"' C-m
     tmux send-keys -t dev:0.4 'cmatrix' C-m
     tmux send-keys -t dev:0.5 'watch -n 30 ${HOME}/mysh/btc.sh price btc' C-m
@@ -30,3 +30,4 @@ then
 
 fi
 tmux attach -t dev
+
---
+++ sh/octopus.py
@@ -0,0 +1,19 @@
+octopusASCII = [
+"      ,'''`.",
+"     /      \ ",
+"     |(@)(@)|",
+"     )      (",
+"    /,'))((`.\ ",
+"   (( ((  )) ))",
+"   ) \ `)(' / ( ",
+]
+
+
+def print_octopus():
+    print()
+    for ol in octopusASCII:
+        print(" "*5 + str(ol))
+    print()
+    
+print_octopus()   
+ 
